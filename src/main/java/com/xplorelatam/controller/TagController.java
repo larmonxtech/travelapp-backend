e221c3ca8b392fbb1e11e59e6757a4a4aeb45456
LarmonxTech <mjlinarezv@yahoo.com>
2026-04-22 21:55:46 -0500

    add Controlador para tag

---
+++ src/main/java/com/xplorelatam/controller/TagController.java
@@ -0,0 +1,40 @@
+package com.xplorelatam.controller;
+
+import com.xplorelatam.model.Tag;
+import com.xplorelatam.service.ITagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tags")
+@RequiredArgsConstructor
+public class TagController {
+    private final ITagService service;
+
+    @GetMapping // GET, POST, PUT, DELETE
+    public List<Tag> findAll() throws Exception{
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Tag findById(@PathVariable("id") Integer id) throws Exception{
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public Tag save(@RequestBody Tag tag) throws Exception{
+        return service.save(tag);
+    }
+
+    @PutMapping("/{id}")
+    public Tag update(@RequestBody Tag tag, @PathVariable("id") Integer id) throws Exception{
+        return service.update(tag, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id) throws Exception{
+        service.delete(id);
+    }
+}
