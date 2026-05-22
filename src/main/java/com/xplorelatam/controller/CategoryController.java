62a973f2820b7012cd46022f7dca3f79e872b7cd
LarmonxTech <mjlinarezv@yahoo.com>
2026-05-22 00:48:51 -0500

    add DTO, Manejo de excepciones personalizadas

--- src/main/java/com/xplorelatam/controller/CategoryController.java
+++ src/main/java/com/xplorelatam/controller/CategoryController.java
@@ -1,8 +1,10 @@
 package com.xplorelatam.controller;
 
+import com.xplorelatam.dto.CategoryDTO;
 import com.xplorelatam.model.Category;
 import com.xplorelatam.service.ICategoryService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -11,40 +13,53 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categories")
 @RequiredArgsConstructor
+@RequestMapping("/categories")
 // @CrossOrigin(origins = "*")
 public class CategoryController {
+    //@Autowired
     private final ICategoryService service;
+    private final ModelMapper modelMapper;
+    
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() throws Exception {
+        // ModelMapper modelMapper = new ModelMapper();
+        // List<CategoryDTO> list = service.findAll().stream().map(e -> new CategoryDTO(e.getIdCategory(), e.getName(), e.getDescription(), e.getStatus())).toList();
+        List<CategoryDTO> list = service.findAll().stream().map(e -> modelMapper.map(e, CategoryDTO.class)).toList();
 
-    @GetMapping // GET, POST, PUT, DELETE
-    public ResponseEntity<List<Category>> findAll() throws Exception{
-        List<Category> list = service.findAll();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Category> findById(@PathVariable("id") Integer id) throws Exception{
-        Category obj =  service.findById(id);
-        return ResponseEntity.ok(obj);
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Integer id) throws Exception {
+        Category obj = service.findById(id);
+
+        return ResponseEntity.ok(modelMapper.map(obj, CategoryDTO.class));
     }
 
     @PostMapping
-    public ResponseEntity<Void> save(@RequestBody Category category) throws Exception{
-        Category obj = service.save(category);
+    public ResponseEntity<Void> save(@RequestBody CategoryDTO dto) throws Exception {
+        Category obj = service.save(modelMapper.map(dto, Category.class));
+
+        //return new ResponseEntity<>(obj, HttpStatus.CREATED);
+        //localhost:9090/categories/4
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getIdCategory()).toUri();
+
         return ResponseEntity.created(location).build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Category> update(@RequestBody Category category, @PathVariable("id") Integer id) throws Exception{
-        Category obj = service.update(category, id);
-        return ResponseEntity.ok(obj);
+    public ResponseEntity<CategoryDTO> update(@PathVariable Integer id, @RequestBody CategoryDTO dto) throws Exception {
+        Category obj = service.update(modelMapper.map(dto, Category.class), id);
+
+        return ResponseEntity.ok(modelMapper.map(obj, CategoryDTO.class));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable("id") Integer id) throws Exception{
+    public ResponseEntity<Void> delete(@PathVariable Integer id) throws Exception {
         service.delete(id);
+
         return ResponseEntity.noContent().build();
     }
+
 }
